ca33e434fba78633206a23f64a11e969987abe37
JHenderson219 <hendersonj219@gmail.com>
2016-12-30 16:42:01 -0800

    Fixe link bug with scripts and change contact section

--- scripts/portfolioscript.js
+++ scripts/portfolioscript.js
@@ -1,29 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(){
-//Thanks to w3Schools for the functions related to the modal.
-//http://www.w3schools.com/howto/howto_css_modals.asp
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+	var emailVisible=false;
+	$("#emailButton").on("click", function(){
+		if (!emailVisible){
+		$("#emailButton").empty().append("Make it disappear! <i class='fa fa-envelope' aria-hidden='true'></i>")
+		$("#emailSpot").empty().append("<h3 class='text-center'> Reach me at Hendersonj219@gmail.com </h3>")
+		}
+	});
 });
